8f473159c1db698ae0ba1454c5231020b18b2a1d
pawelkozolub <pkozolub@gmail.com>
2023-08-27 20:54:43 +0200

    AppConfig class

---
+++ src/main/java/pl/coderslab/configuration/AppConfig.java
@@ -0,0 +1,16 @@
+package pl.coderslab.configuration;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = "pl.coderslab")
+public class AppConfig implements WebMvcConfigurer {
+    // empty body
+}
