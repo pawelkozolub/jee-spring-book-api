8aeea0080ddb5cd4ad287e4f260b1112c634567b
pawelkozolub <pkozolub@gmail.com>
2023-08-29 22:28:51 +0200

    Service > BookService interface

--- src/main/java/pl/coderslab/service/BookService.java
+++ src/main/java/pl/coderslab/service/BookService.java
@@ -7,7 +7,7 @@ import java.util.Optional;
 
 public interface BookService {
     List<Book> getBooks();
-//    Optional<Book> get(Long id);
+    Optional<Book> get(Long id);
 //    void add(Book book);
 //    void delete(Long id);
 //    void update(Book book);
