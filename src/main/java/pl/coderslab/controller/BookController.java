433699880a472efc05539320d5da3ccffbc9ff07
pawelkozolub <pkozolub@gmail.com>
2023-08-29 22:30:02 +0200

    Controller > BookController

--- src/main/java/pl/coderslab/controller/BookController.java
+++ src/main/java/pl/coderslab/controller/BookController.java
@@ -1,6 +1,7 @@
 package pl.coderslab.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import pl.coderslab.model.Book;
@@ -22,6 +23,11 @@ public class BookController {
     @GetMapping
     public List<Book> getList() {
         return bookService.getBooks();
+    }
+
+    @GetMapping("/{id}")
+    public Book getBook(@PathVariable(name="id") Long id) {
+        return bookService.get(id).orElse(null);
     }
 
     @RequestMapping("/helloBook")
