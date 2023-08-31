d2e534e849621e58500b4388af84b39c744fc30f
pawelkozolub <pkozolub@gmail.com>
2023-08-31 23:15:26 +0200

    Service > BookService Interphase > add(Book book)

--- src/main/java/pl/coderslab/service/BookService.java
+++ src/main/java/pl/coderslab/service/BookService.java
@@ -8,7 +8,7 @@ import java.util.Optional;
 public interface BookService {
     List<Book> getBooks();
     Optional<Book> get(Long id);
-//    void add(Book book);
+    void add(Book book);
 //    void delete(Long id);
 //    void update(Book book);
 }
